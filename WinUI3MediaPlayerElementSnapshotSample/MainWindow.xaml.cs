fa2b6fba6a9b41beb6e4b8849e4c49e4f16696cc
mifumi323 <mifumi323@gmail.com>
2025-02-20 22:01:17 +0900

    メディアプレイヤーとスナップショット保存機能追加

--- WinUI3MediaPlayerElementSnapshotSample/MainWindow.xaml.cs
+++ WinUI3MediaPlayerElementSnapshotSample/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Microsoft.Graphics.Canvas;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using System;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Pickers;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -28,9 +20,29 @@ namespace WinUI3MediaPlayerElementSnapshotSample
             this.InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private async void PhotoButton_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            var savePicker = new FileSavePicker();
+            savePicker.FileTypeChoices.Add("PNG", [".png"]);
+
+            // ‚±‚ÌƒLƒ‚‚¢‚¨‚Ü‚¶‚È‚¢–³‚­‚È‚é‚Æ‚¢‚¢‚È(https://qiita.com/hayashida-katsutoshi/items/21b8f99c462c01bb7c6d)
+            InitializeWithWindow.Initialize(savePicker, WindowNative.GetWindowHandle(this));
+
+            var file = await savePicker.PickSaveFileAsync();
+            if (file != null)
+            {
+                int naturalVideoWidth = (int)Player.MediaPlayer.PlaybackSession.NaturalVideoWidth;
+                int naturalVideoHeight = (int)Player.MediaPlayer.PlaybackSession.NaturalVideoHeight;
+
+                CanvasDevice canvasDevice = CanvasDevice.GetSharedDevice();
+
+                var frameServerDest = new SoftwareBitmap(BitmapPixelFormat.Rgba8, naturalVideoWidth, naturalVideoHeight, BitmapAlphaMode.Ignore);
+                using var inputBitmap = CanvasBitmap.CreateFromSoftwareBitmap(canvasDevice, frameServerDest);
+                Player.MediaPlayer.CopyFrameToVideoSurface(inputBitmap);
+
+                var photoPath = file.Path;
+                await inputBitmap.SaveAsync(photoPath, CanvasBitmapFileFormat.Png);
+            }
         }
     }
 }
